72377a26b42920b56b46446360373ad444179383
Now18 <nicolasmartins803@gmail.com>
2023-01-28 17:50:54 -0300

    Correções na função de remover paciente

--- js/remove.js
+++ js/remove.js
@@ -1,5 +1,3 @@
-var paciente = document.querySelectorAll(".paciente");
-function remover(paciente){
-    this.remove(paciente);
-}
-paciente.addEventListener("dblclick", remover);
+document.querySelector("#tabela-pacientes").addEventListener("dblclick", function(event){
+    event.target.parentNode.remove();
+});
